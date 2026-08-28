ac8a691604ea7c114e52f4f790cdc7c930f6e985
Juan Andrés <juan.rivera7@udea.edu.co>
2026-08-27 19:14:37 -0500

    NUEVO COMMIT

--- app/components/molecules/HeroText/index.tsx
+++ app/components/molecules/HeroText/index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
       </div>
       <div>
         <p className='text-blanco text-lg mt-4'>
-          TEXTO EDITADO
+          NUEVO TEXTO CON COMMMIT NUEVO
         </p>
       </div>
       <div>
